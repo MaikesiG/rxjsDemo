54bf14a5018a02379b0d70975fddc25e7822913d
J1NTA <snow93star@gmail.com>
2021-03-07 02:13:39 -0500

    baidu-input

--- src/app/test-rxjs/test-rxjs.component.ts
+++ src/app/test-rxjs/test-rxjs.component.ts
@@ -1,22 +1,67 @@
 import { Component, OnInit, ChangeDetectionStrategy, Injectable, ViewChild, ElementRef, AfterViewInit, ChangeDetectorRef } from '@angular/core';
-import { fromEvent, Observable, of } from 'rxjs';
+import { from, fromEvent, Observable, of, throwError } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
-import { debounceTime, distinctUntilChanged, map, pluck, switchMap } from 'rxjs/operators';
+import { catchError, debounceTime, distinctUntilChanged, map, pluck, retry, switchMap } from 'rxjs/operators';
+import jsonpG from 'jsonp-good';
+
+interface JsonpRes {
+  q:string;
+}
 @Injectable()
 class WikiService{
+  readonly baiduUrl = 'https://www.baidu.com/sugrec'
   readonly url = 'https://zh.wikipedia.org/w/api.php?action=opensearch&format=json&limit=5&origin=*&search=';
   list(keyword:string): Observable<string[]> {
     return ajax.getJSON(this.url+keyword)
     .pipe(map( item => item[1]));
   }
+
+  // Promise
+  // baiduList(keyword:string): Promise<JsonpRes[]> {
+  //   return jsonpG({
+  //     url: this.baiduUrl,
+  //     params: {
+  //       prod: 'pc',
+  //       from: 'pc_web',
+  //       wd:keyword,
+  //     },
+  //     funcName:'jQuery110203052522071732855_1604236886158',
+  //   })
+  //   .then((res : {g:JsonpRes[]}) => res.g);
+  // }
+
+  baiduList(keyword:string): Observable<JsonpRes[]> {
+    return from(
+      jsonpG({
+        url: this.baiduUrl,
+        params: {
+          prod: 'pc',
+          from: 'pc_web',
+          wd:keyword,
+        },
+        funcName:'jQuery110203052522071732855_1604236886158',
+      }).then((res : {g:JsonpRes[]}) => {
+        return res.g;
+      })
+    )
+  }
+
 }
 @Component({
   selector: 'app-test-rxjs',
   template: `
+    <h1>wiki</h1>
     <div class="autocomplete">
       <input #input class="form-control" placeholder="search..." />
       <ul class="list-group mt-2">
         <li class="list-group-item" *ngFor="let item of list">{{item}}</li>
+      </ul>
+    </div>
+    <h1>baidu</h1>
+    <div class="autocomplete">
+      <input #baiduInput class="form-control" placeholder="search..." />
+      <ul class="list-group mt-2">
+        <li class="list-group-item" *ngFor="let baiduItem of baiduList">{{baiduItem.q}}</li>
       </ul>
     </div>
   `,
@@ -27,7 +72,10 @@ class WikiService{
 })
 export class TestRxjsComponent implements OnInit, AfterViewInit {
   list:string[] = [];
+  baiduList:JsonpRes[] = [];
+  // list$:Observable<string[]>;
   @ViewChild('input', {static:true}) private inputEl:ElementRef;
+  @ViewChild('baiduInput',{static:true}) private baiduInputEl:ElementRef;
   constructor(
     private wikiServe:WikiService,
     private cdr: ChangeDetectorRef
@@ -35,6 +83,12 @@ export class TestRxjsComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit(): void {
+    // this.wikiServe.baiduList('我')
+    // .then(
+    //   data=>{
+    //     console.log('a',data)
+    //   }
+    // )
   }
   ngAfterViewInit():void {
     fromEvent(this.inputEl.nativeElement,'input')
@@ -44,12 +98,35 @@ export class TestRxjsComponent implements OnInit, AfterViewInit {
       distinctUntilChanged(),
       switchMap((value:string) =>  {
         return value? this.wikiServe.list(<string>value): of([]);
-      })
+      }),
+      catchError(err =>throwError(err)),
+      retry(2),
     )
     .subscribe( data => {
-      console.log(data);
+      console.log('wikiInput',data);
       this.list = data;
       this.cdr.markForCheck();
-    })
+    }, error =>{
+      console.log('err',error);
+    });
+
+    fromEvent(this.baiduInputEl.nativeElement,'input')
+    .pipe(
+      debounceTime(500),
+      pluck('target','value'),
+      distinctUntilChanged(),
+      switchMap((value:string) =>  {
+        return value? this.wikiServe.baiduList(<string>value): of([]);
+      }),
+      catchError(err =>throwError(err)),
+      retry(2),
+    )
+    .subscribe( data => {
+      console.log('baiduInput',data);
+      this.baiduList = data;
+      this.cdr.markForCheck();
+    }, error =>{
+      console.log('err',error);
+    });
   }
 }
